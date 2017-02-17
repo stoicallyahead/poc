10880d42fd89bc07420551c13e5ab843955cebbd
anuvrat <anuvrat.rastogi@sap.com>
2017-02-18 01:44:13 +0530

    initial commit

---
+++ TestMerge/src/StringTest.java
@@ -0,0 +1,14 @@
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		
+		String first = "Anuvrat";
+		String last = "Rastogi";
+		
+		System.out.println("First name is: " + first + " and last name is: " + last);
+		
+		
+	}
+
+}
