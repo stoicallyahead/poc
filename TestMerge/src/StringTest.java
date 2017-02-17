3d89df2237744ff363fed3eb97410c01d8e31050
anuvrat <anuvrat.rastogi@sap.com>
2017-02-18 01:49:53 +0530

    order change

--- TestMerge/src/StringTest.java
+++ TestMerge/src/StringTest.java
@@ -3,10 +3,11 @@ public class StringTest {
 
 	public static void main(String[] args) {
 		
-		String first = "Tulika";
 		String last = "Rastogi";
+		String first = "Tulika";
 		
 		System.out.println("Last name is: " + last + " and first name is: " + first);
+		System.out.println("merge conflict");
 		
 		
 	}
