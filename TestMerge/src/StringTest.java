813cd507c6575e5418d0e54bb78872b26461db45
anuvratrastogi <anuvrat.rastogi@gmail.com>
2017-03-17 23:51:29 +0530

    one

--- TestMerge/src/StringTest.java
+++ TestMerge/src/StringTest.java
@@ -4,7 +4,7 @@ public class StringTest {
 	public static void main(String[] args) {
 		
 		String last = "Rastogi";
-		String first = "Tulika";
+		String first = "Anuvrat";
 		
 		System.out.println("Last name is: " + last + " and first name is: " + first);
 		System.out.println("merge conflict");
