42cb957d622120b93a863f98eff13c30e5dbb473
anuvratrastogi <anuvrat.rastogi@gmail.com>
2017-03-19 15:25:08 +0530

    removes some

--- TestMerge/src/StringTest.java
+++ TestMerge/src/StringTest.java
@@ -14,11 +14,7 @@ public class StringTest {
 		System.out.println("local");
 		
 		System.out.println("local");
-		
-		
-		System.out.println("local");
-		
-		System.out.println("local");
+
 	}
 
 }
